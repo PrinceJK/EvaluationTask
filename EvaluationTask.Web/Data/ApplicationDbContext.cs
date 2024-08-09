097cf525db23a82fca5525c0c485159c094f3693
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-08-09 22:36:38 +0100

    added ApplicationDbContext class

---
+++ EvaluationTask.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EvaluationTask.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaluationTask.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+	{
+	}
+
+	public DbSet<CancellationRequest> CancellationRequests { get; set; }
+}
