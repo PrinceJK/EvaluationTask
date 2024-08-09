59a936220f8f5c9deb63528c7334b66719d246d0
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-08-09 22:33:50 +0100

    created model and installed database

---
+++ EvaluationTask.Web/Models/CancellationRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvaluationTask.Web.Models;
+
+public class CancellationRequest
+{
+	public int Id { get; set; }
+
+	[Required]
+	[StringLength(100)]
+	public string RequestorName { get; set; }
+
+	[DataType(DataType.Date)]
+	public DateTime RequestDate { get; set; }
+
+	[StringLength(100)]
+	public string Status { get; set; }
+
+	public decimal? FeeAmount { get; set; }
+}
