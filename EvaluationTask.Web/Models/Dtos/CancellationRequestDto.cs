9957ad6142e4c7f4109728db3988dc153d759982
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-08-12 19:54:19 +0100

    created dto for CancellationRequests

---
+++ EvaluationTask.Web/Models/Dtos/CancellationRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using EvaluationTask.Web.Models.Enums;
+
+namespace EvaluationTask.Web.Models.Dtos;
+
+public class CancellationRequestDto
+{
+    public string RequestorName { get; set; }
+    public DateTime RequestDate { get; set; }
+    public Status Status { get; set; }
+    public decimal FeeAmount { get; set; }
+    public string Message { get; set; }
+}
